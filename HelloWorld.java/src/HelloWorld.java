62f26c19a9887c11e06cd877eed83373060e940c
Varun <vdharmala@gmail.com>
2017-07-24 21:26:07 -0700

    (Added HelloWorld program)

---
+++ HelloWorld.java/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World"); 
+		
+		int x = 3;
+		int y = 4; 
+		int z = x + y;
+		
+		System.out.println(z);
+		
+		
+
+	}
+
+}
